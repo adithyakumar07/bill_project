f66e8933c02519d1a54c9f4df73366f4f4e15af4
AdithyaKumar <kumaradithya537@gmail.com>
2025-05-04 11:50:52 +0530

    bill_code

---
+++ bill_code.py
@@ -0,0 +1,43 @@
+iPHONE_16_mobile_cost =90000
+
+SGST = 18 / 100
+CGST = 18 / 100
+
+
+print("------- We are selling iPhone mobiles --------")
+print("-------Each costs 90000 INR ---------------")
+
+required_items = int(input("No. of required items: "))
+ 
+# Apply discount based on quantity
+if 2 <= required_items <= 5:
+    discount = 9 / 100
+elif 6 <= required_items <= 10:
+    discount = 19 / 100
+elif 11 <= required_items <= 20:
+    discount = 19 / 100
+elif 21 <= required_items <= 40:
+    discount = 24 / 100
+elif required_items >= 41:
+    discount = 29 / 100
+else:
+    discount =  2 / 100
+
+iPhone_total_cost = required_items * iPHONE_16_mobile_cost
+iPhone_SGST = iPHONE_16_mobile_cost * SGST
+iPhone_CGST = iPHONE_16_mobile_cost * CGST
+iPhone_discount_amount = iPHONE_16_mobile_cost * discount
+
+#total_cost
+total_cost = iPHONE_16_mobile_cost + iPhone_SGST + iPhone_CGST - iPhone_discount_amount
+
+# Output
+print("------------ Billing Summary --------------------")
+print("     Total mobile Cost        : ₹",iPhone_total_cost)
+print("     SGST (18%)                : ₹",iPhone_SGST)
+print("     CGST (18%)               : ₹",iPhone_CGST)
+print("     Discount Applied(" ,discount,") : ₹",iPhone_discount_amount)
+print("-------------------------------------------------")
+print("     Total Payable Amount     : ₹",total_cost)
+print("-------------------------------------------------")
+print(".<.<.<    THANK_YOU AND VISIT_AGAIN    >.>.>.")
